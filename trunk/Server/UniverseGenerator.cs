46083d86702a46be510aaf37865afe2b62fcd5e5
mckaysalisbury@gmail.com <mckaysalisbury@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-29 03:11:15 +0000

    Added a universe generator

---
+++ trunk/Server/UniverseGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    /// <summary>
+    /// Generates a new universe
+    /// </summary>
+    public static class UniverseGenerator
+    {
+        /// <summary>
+        /// Generates a new universe
+        /// </summary>
+        /// <returns>a newly generated universe</returns>
+        public static Universe Generate()
+        {
+            var universe = new Universe();
+
+            return universe;
+        }
+    }
+}
