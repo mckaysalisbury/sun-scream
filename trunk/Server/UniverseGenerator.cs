5e4169b5fdfd6321971454b85c59795321bbb680
mckaysalisbury@gmail.com <mckaysalisbury@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-29 19:12:22 +0000

    It's full of stars!

--- trunk/Server/UniverseGenerator.cs
+++ trunk/Server/UniverseGenerator.cs
@@ -19,8 +19,11 @@ namespace Server
         {
             var universe = new Universe();
 
-            universe.AddEntity(new StarSystem("The first Star"), new Vector2(10, 10));
-            universe.AddEntity(new StarSystem("The second Star"), new Vector2(-10, -10));
+            universe.AddEntity(new StarSystem("Sol"), new Vector2(0,0));
+            universe.AddEntity(new StarSystem("Sirius"), new Vector2(30, 5));
+            universe.AddEntity(new StarSystem("Procyon"), new Vector2(-40, 20));
+            universe.AddEntity(new StarSystem("Deneb"), new Vector2(100, -100));
+            universe.AddEntity(new StarSystem("Sirius"), new Vector2(-100, 100));
 
             // Remove these later
             //universe.AddEntity(universe.GenerateShip(new Player(null) { Name = "Test" }), universe.GetSpawnLocation());
