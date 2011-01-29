970c0f1f393a6a69acecd8bf35b5329e1d0c5798
mckaysalisbury@gmail.com <mckaysalisbury@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-29 22:59:18 +0000

    Releasing tractors

---
+++ trunk/Server/Commands/ReleaseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    class ReleaseCommand : Command
+    {
+        public override string Execute(Player source, string arguments)
+        {
+            var result = source.Controlling.Release();
+            if (result == null)
+            {
+                return "Nothing available to release";
+            }
+            else
+            {
+                return string.Format("Releasing #{0}={1}:{2}", result.Id, result.Name, result.GetClientType());
+            }
+        }
+    }
+}
