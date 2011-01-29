8fa374d4d3e3ce5a66146d23804c21a4e273bd84
sambena@gmail.com <sambena@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-29 05:02:06 +0000

    Created Asteroid.cs

---
+++ trunk/Server/GameObjects/Asteroid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    /// <summary>
+    /// Stores information about a ship
+    /// </summary>
+    public class Asteroid : Entity
+    {
+        private const float size = 0.1f;
+
+        public Asteroid(int id, string name, float x, float y) : base(id, name, size, size)
+        {
+        }
+
+        internal override EntityUpdateType GetClientType()
+        {
+            return EntityUpdateType.Ship;
+        }
+    }
+}
