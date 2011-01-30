735d01228362d453bc7336773b2861cb71025dae
tyrecius@gmail.com <tyrecius@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-30 10:57:09 +0000

    Naming done.

--- Server/Commands/SetNameCommand.cs
+++ Server/Commands/SetNameCommand.cs
@@ -10,6 +10,8 @@ namespace Server
         public override string Execute(Player source, string arguments)
         {
             source.Name = arguments;
+            if (source.Controlling != null)
+                source.Controlling.Name = source.Name;
             return String.Format("Name set to {0}", source.Name);
         }
     }
