57d69398736018a88f07a4b10dd375c2939a3d39
mckaysalisbury@gmail.com <mckaysalisbury@gmail.com@70277e72-2734-474d-66fd-5a4d2d7ea173>
2011-01-30 05:09:56 +0000

    Hitchhiker.cs starting

---
+++ trunk/Server/GameObjects/TowItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+
+namespace Server
+{
+    public abstract class TowItem : Entity
+    {
+        protected const float size = 0.1f;
+        protected const float speedFactor = 0.1f;
+
+        public Ship LastShip = null;
+
+        public TowItem() : base(string.Empty)
+        {
+        }
+
+        protected override Fixture GetFixture(World world)
+        {
+            var fixture = FixtureFactory.CreateCircle(world, size, 1);
+            fixture.Body.BodyType = BodyType.Dynamic;
+            fixture.Body.LinearVelocity = new Vector2(((float)ScreamMath.Random.NextDouble() - 0.5f) * speedFactor, ((float)ScreamMath.Random.NextDouble() - 0.5f) * speedFactor);
+            return fixture;
+        }
+
+        public override bool IsTractorable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
